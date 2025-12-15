3bc1c3cf5dc1e512502b99b630a204288a50d9a3
Marco Bacci <marcobacci94@gmail.com>
2025-12-15 16:02:11 +0100

    Feat: milestone 3 completed

--- src/main.ts
+++ src/main.ts
@@ -23,3 +23,48 @@ type Actress = Person & {
     | "South Korean"
     | "Chinese";
 };
+
+function isActress(data: unknown): data is Actress {
+  return (
+    typeof data === "object" &&
+    data !== null &&
+    "id" in data &&
+    typeof data.id === "number" &&
+    "name" in data &&
+    typeof data.name === "string" &&
+    "birth_year" in data &&
+    typeof data.birth_year === "number" &&
+    "death_year" in data &&
+    typeof data.death_year === "number" &&
+    "biography" in data &&
+    typeof data.biography === "string" &&
+    "image" in data &&
+    typeof data.image === "string" &&
+    "most_famous_movies" in data &&
+    data.most_famous_movies instanceof Array &&
+    data.most_famous_movies.length === 3 &&
+    data.most_famous_movies.every((m) => typeof m === "string") &&
+    "awards" in data &&
+    typeof data.awards === "string" &&
+    "nationality" in data &&
+    typeof data.nationality === "string"
+  );
+}
+
+async function getActress(id: number): Promise<Actress | null> {
+  try {
+    const response = await fetch(`http://localhost:3333/actresses/${id}`);
+    const data: unknown = await response.json();
+    if (!isActress(data)) {
+      throw new Error("formato dati non valido");
+    }
+    return data;
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error("errore durante il recupero di attrice");
+    } else {
+      console.error("errore sconosciuto");
+    }
+    return null;
+  }
+}
